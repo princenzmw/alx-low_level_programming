f2cf48d936fd50ce0c3dae4bb56bb0a877870ed7
princenzmw <princenzmw@gmail.com>
2023-07-14 05:06:44 +0300

    Hello World! Welcome to C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -9,7 +9,7 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
+
 	return (0);
 }
 
