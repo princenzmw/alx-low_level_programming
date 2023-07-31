11fd458a845d3fd55a3af7ca1dd1ec07beffe40c
princenzmw <princenzmw@gmail.com>
2023-07-31 20:00:22 +0300

    Last fix-up

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,20 +5,16 @@
  * to by @s with the constant byte @n
  * @n: bytes of the memory area pointed to by @s
  * @s: with the constant byte
- * @b: the character to fill the area
+ * @c: the character to fill the area
  * Return: a pointer to the memory area @s
  */
 
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s;
-	unsigned char value = b;
+	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-	{
 		memory[index] = value;
-	}
-
-	return (s);
+	return (memory);
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,19 +4,26 @@
 /**
  * print_diagsums - Prints the sum of the two diagonals
  *                  of a square matrix of integers.
- * @a: The matrix of integers. (input pointer)
+ * @a: The matrix of integers.
  * @size: The size of the matrix.
  */
 void print_diagsums(int *a, int size)
 {
 	int index, sum1 = 0, sum2 = 0;
 
-	for (index = 0; index < (size * size); index++)
+	for (index = 0; index < size; index++)
 	{
-		if (index % (size + 1) == 0)
-			S1 += a[index];
-		if (index % (size - 1) == 0 && index != 0 && index < size * size - 1)
-			S2 += a[index];
+		sum1 += a[index];
+		a += size;
 	}
+
+	a -= size;
+
+	for (index = 0; index < size; index++)
+	{
+		sum2 += a[index];
+		a -= size;
+	}
+
 	printf("%d, %d\n", sum1, sum2);
 }
