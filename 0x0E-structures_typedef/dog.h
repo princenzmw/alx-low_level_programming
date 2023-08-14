ef6b6933ede0002556691be85e4fe924aa781f01
princenzmw <princenzmw@gmail.com>
2023-08-14 18:10:01 +0300

    Hot fix up for betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,6 @@ struct dog
 /**
  * dog_t - Typedef for defining dog_t as a new name for the type struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
