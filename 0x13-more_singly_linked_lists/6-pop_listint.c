ae0f7277138fabddcb52a648af306707f6fa19f9
princenzmw <princenzmw@gmail.com>
2023-08-29 01:00:16 +0300

    fixing some code on task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,13 +11,15 @@ int pop_listint(listint_t **head)
 	int value;
 	listint_t *temp;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
 
 	value = (*head)->n;
-	temp = *head;
+	*temp = *head;
 	*head = (*head)->next;
 	free(temp);
+
+	return (value);
 }
