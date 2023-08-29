4a29d57c259d313530640dbf79cf531ee8581f88
princenzmw <princenzmw@gmail.com>
2023-08-29 14:58:17 +0300

    Fixing some errors in code task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,60 @@
 #include "lists.h"
 
+listint_t *add_nodeint(listint_t **head, const int n);
+
 /**
- * print_listint_safe - Prints all elements of a listint_t list safely.
+ * print_listint_safe - Prints a listint_t linked list.
  * @head: A pointer to the head of the list.
- * Return: The number of nodes.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
-	size_t node_count = 0;
+	size_t count = 0;
+	const listint_t *current = head;
+	const listint_t *visitedNodes[1024];
 
-	while (slow && fast && fast->next)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		slow = slow->next;
-		fast = fast->next;
-		node_count++;
+		size_t i;
 
-		if (slow == fast)
+		for (i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			break;
+			if (current == visitedNodes[i])
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				printf("Loop starts at [%p] %d\n", (void *)current, current->n);
+				return (count);
+			}
 		}
+
+		visitedNodes[count++] = current;
+		printf("[%p] %d\n", (void *)current, current->n);
+		current = current->next;
 	}
 
-	return (node_count);
+	return (count);
+}
+
+#include "lists.h"
+
+/**
+ * add_nodeint - Adds a new node at the beginning of listint_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @n: The value to add to the new node.
+ *
+ * Return: A pointer to the new head of the list.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
