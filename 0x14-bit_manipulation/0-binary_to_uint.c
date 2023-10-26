b61ef113069a177ac7e5b4c6f87d4248d8f4b503
princenzmw <princenzmw@gmail.com>
2023-10-26 21:38:20 +0300

    Completed Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-
-	if (!b)
-		return (0);
-
-	while (*b == '0' || *b == '1')
-	{
-		decimal = (decimal << 1) | (*b - '0');
-		b++;
-	}
-
-	return ((*b == '\0') ? decimal : 0);
-}
-/*
-*unsigned int binary_to_uint(const char *b)
-*{
+	int count;
 	unsigned int decimalNum = 0;
 
 	if (!b)
@@ -32,15 +17,14 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	}
 
-	while (*b)
+	for (count = 0; b[count]; count++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[count] < '0' || b[count] > '1')
 		{
 			return (0);
 		}
-		decimalNum = (decimalNum << 1) | (*b - '0');
-		b++;
+		decimalNum = 2 * decimalNum + (b[count] - '0');
 	}
+
 	return (decimalNum);
 }
-*/
