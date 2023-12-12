51c63aba3a32f5594656207ef13e986567fb7d0d
princenzmw <princenzmw@gmail.com>
2023-12-12 04:11:56 +0300

    working on it

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -P .. https://github.com/princenzmw/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libfinal.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+exec ./gm 9 8 10 24 75 9 "$@"
