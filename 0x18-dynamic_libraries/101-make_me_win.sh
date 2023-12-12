2f05723a80cb6516edfedb966267d4265c3d7b39
princenzmw <princenzmw@gmail.com>
2023-12-12 04:40:50 +0300

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.githubusercontent.com/princenzmw/alx-low_level_programming/main/0x18-dynamic_libraries/libfinal.so
-export LD_PRELOAD="$PWD/libfinal.so"
+export LD_PRELOAD="$PWD/../libfinal.so"
